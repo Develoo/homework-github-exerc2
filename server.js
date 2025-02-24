9538d43111300536781fc23fd1771569b226a369
Develoo <brahmii.kathemm@gmail.com>
2025-02-24 12:58:03 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Route principale
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+// Démarrer le serveur
+app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+});
